0b48b0da1f7136c894c0cab486ab75ee2a80993c
shashvatkawali27-bit <shashvatkawali27@eng.rizvi.edu.in>
2026-02-17 22:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder:Shashvat Nilesh Kawali
+# Date:17 feburary 2026
 
 print("--- Extracting Words from Text File ---\n")
+with open("story.txt") as f:
+    content=f.read().split()
+
+length=int(input("Enter Length of Words:"))
+
+list=[]
+for i in content:
+    if(len(i)==length):
+        list.append(i)
+
+list=set(list)
+ #list=list(list)
+print(list)
 
